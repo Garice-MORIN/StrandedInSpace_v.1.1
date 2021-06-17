7d941ea0a65e88725883fba06200c2c5045d2781
Victor SIXOU <73958557+EPT-Sucre@users.noreply.github.com>
2021-06-17 15:31:26 +0200

    Save starting to look like a real function tbh

--- SIS-2.0/SIS-2.0/Assets/Scripts/Connection.cs
+++ SIS-2.0/SIS-2.0/Assets/Scripts/Connection.cs
@@ -3,30 +3,75 @@ using UnityEngine.UI;
 using MySql.Data.MySqlClient;
 using System.IO;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-
+using CryptSharp;
+using TreeEditor;
 
 public class Connection : MonoBehaviour
 {
-    string db = "server=localhost;userid=id16853390_root;password=KIujyh84000!; database=id16853390_players	";
     public InputField login;
     public InputField password;
     private string _login;
     private string _password;
-    public Text ErrorMessage;
+    public List<bool>[] Trees;
 
-    public void On_Click()
+    public string[] GetInfos(string file)
     {
-        _login = login.text;
-        _password = password.text;
-        MySqlConnection con = new MySqlConnection(db);
-        try
-        { con.Open(); }
-        catch (IOException Ex)
+        string[] arr = new string[2];
+        using (StreamReader sr = new StreamReader(file))
         {
-            ErrorMessage.text = Ex.ToString()+" Please try again.";
+            string line = sr.ReadLine();
+            arr[0] = line.Split(',')[0];
+            arr[1] = line.Split(',')[1];
         }
+
+        return arr;
+    }
+
+    public List<bool> GetData(string file, int n)
+    {
+        List<bool> dataList = new List<bool>();
+        using (StreamReader sr = new StreamReader(file))
+        {
+            for (int i = 0; i < n-1; i++)
+            {
+                sr.ReadLine();
+            }
+
+            string[] data = sr.ReadLine().Split(' ');
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i]=="0")
+                {
+                    dataList.Add(true);
+                }
+                dataList.Add(false);
+            }
+        }
+
+        return dataList;
+    }
+    public List<bool>[] On_Click()
+    {
+        string file = "../../StandAlone/infos.txt";
+        string[] infos = GetInfos(file);
+        _login = login.ToString();
+        _password = password.ToString();
+        if (infos[0] == _login && Crypter.CheckPassword(_password, infos[1]))
+        {
+            List<bool> characterTree = GetData(file, 1);
+            List<bool> towerTree = GetData(file, 2);
+            List<bool> trapTree = GetData(file, 3);
+            Trees = new List<bool>[3];
+            Trees[0] = characterTree;
+            Trees[1] = towerTree;
+            Trees[2] = trapTree;
+            return Trees;
+        }
+        Console.Error.WriteLine("Erreur, pas le bon mot de passe/username, merci de réessayer");
+        return Trees;
     }
 }
