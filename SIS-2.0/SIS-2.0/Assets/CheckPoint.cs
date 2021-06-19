16dcd97315748ee117e632bf2bbbe0ce992bf1d6
Garice-MORIN <76947301+Garice-MORIN@users.noreply.github.com>
2021-06-19 07:02:14 +0200

    PUTAIN CA MARCHE

--- SIS-2.0/SIS-2.0/Assets/CheckPoint.cs
+++ SIS-2.0/SIS-2.0/Assets/CheckPoint.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
 	public Transform coreTransform;
-
 	private void OnTriggerEnter(Collider other) {
-		if(other.GetComponent<EnemyMovement>().type != Type.FLYING) {
+		if(other.tag == "Enemy" && other.GetComponent<EnemyMovement>().type != Type.FLYING) {
 			other.GetComponent<EnemyMovement>().SetGoal(coreTransform);
 		}
 	}
--- SIS-2.0/SIS-2.0/Assets/Scripts/EnemyMovement.cs
+++ SIS-2.0/SIS-2.0/Assets/Scripts/EnemyMovement.cs
@@ -121,23 +121,20 @@ public class EnemyMovement : MonoBehaviour
         if(type == Type.FLYING) {
             navMesh.areaMask = ALL;
 		}
-        else {
-            int rnd = UnityEngine.Random.Range(1, 5);
-            switch (rnd) {
-                case 1:
-                    navMesh.areaMask = GG;
-                    break;
-                case 2:
-                    navMesh.areaMask = DG;
-                    break;
-                case 3:
-                    navMesh.areaMask = DD;
-                    break;
-                case 4:
-                    navMesh.areaMask = GD;
-                    break;
-            }
-        }
-        
+        int rnd = UnityEngine.Random.Range(1, 5);
+        switch(rnd) {
+            case 1:
+                navMesh.areaMask = GG;
+                break;
+            case 2:
+                navMesh.areaMask = DG;
+                break;
+            case 3:
+                navMesh.areaMask = DD;
+                break;
+            case 4:
+                navMesh.areaMask = GD;
+                break;
+		}
 	}
 }
